315839480c3cf478099f06ab076403a8cc27da98
Shohboz Xurramov <shohboztuit@gmail.com>
2023-09-03 22:28:24 +0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿namespace misol6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MusicPlayer player = new MusicPlayer();
+            player.Play();
+            player.Pause();
+            player.Stop();
+        }
+    }
+    public interface IPlayer
+    {
+        void Play();
+        void Pause();
+        void Stop();
+
+    }
+    public class MusicPlayer : IPlayer
+    {
+        public void Play()
+        {
+            Console.WriteLine("Playing music");
+        }
+        public void Pause()
+        {
+            Console.WriteLine("Pausing music");
+        }
+        public void Stop()
+        {
+            Console.WriteLine("Stopping music");
+        }
+    }
+}
